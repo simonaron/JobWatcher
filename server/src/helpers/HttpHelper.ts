4b480bf363a7037f791b016501a474422eb7a191
CORP\arm <aron.simon@sigmatechnology.se>
2017-11-16 17:36:48 +0100

    jenkins example

---
+++ server/src/helpers/HttpHelper.ts
@@ -0,0 +1,75 @@
+import * as request from 'request';
+
+export interface HttpError {
+    url: string,
+    statusCode: number,
+    message: string 
+}
+
+export class HttpHelper {
+    static async post(url: string, headers?: any, from?: any) {
+        return (new Promise((
+                resolve: (response: any) => any, 
+                reject: (error: HttpError) => void
+            ) => {
+                request.post({
+                    url: url,
+                    json: true,
+                    headers: headers || {},
+                    form: from || {}
+                }, function (error: any, response: any, body: any) {
+                    if (!error && response.statusCode === 201) {
+                        resolve(body);
+                    } else if(response) {
+                        reject({
+                            url: url,
+                            statusCode: response.statusCode, 
+                            message: error
+                        });
+                    } else {
+                        reject({
+                            url: url,
+                            statusCode: undefined, 
+                            message: error
+                        });
+                    }
+                });
+            }).catch((error: HttpError) => {
+                throw error;
+            })
+        );
+    }
+
+    static async get(url: string, headers?: any, from?: any) {
+        return (new Promise((
+                resolve: (response: any) => any, 
+                reject: (error: HttpError) => void
+            ) => {
+                request.get({
+                    url: url,
+                    json: true,
+                    headers: headers || {},
+                    form: from || {}
+                }, function (error: any, response: any, body: any) {
+                    if (!error && response.statusCode === 200) {
+                        resolve(body);
+                    } else if(response) {
+                        reject({
+                            url: url,
+                            statusCode: response.statusCode, 
+                            message: error
+                        });
+                    } else {
+                        reject({
+                            url: url,
+                            statusCode: undefined, 
+                            message: error
+                        });
+                    }
+                });
+            }).catch((error: HttpError) => {
+                throw error;
+            })
+        );
+    }
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,20 @@
-import * as Express from 'express'
 
-let App = Express();
+import * as Express from 'express';
+import { HttpHelper } from './helpers/HttpHelper';
+// let App = Express();
 
-App.get("/valami/:id", (req: Express.Request, res: Express.Response) => {
-    console.log("alma")
-    res.send('Szia!' + req.params.id);
-});
+// App.get("/valami/:id", (req: Express.Request, res: Express.Response) => {
+//     console.log("alma")
+//     res.send('Szia!' + req.params.id);
+// });
 
-App.listen(3000, () => {
-    console.log("listening on port 3000")
-});
+// App.listen(3000, () => {
+//     console.log("listening on port 3000")
+// });
+async function main() {
+let kiscica = await HttpHelper.get(
+    'https://tcc-titansim.rnd.ki.sw.ericsson.se/job/SYSF_SFTF_DEV/api/json?tree=lastSuccessfulBuild[number]')
+console.log(kiscica);
+}
 
+main();
