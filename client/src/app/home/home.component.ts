09a260460e36c5cc725c3e0715b6c21596d1d16a
CORP\arm <aron.simon@sigmatechnology.se>
2017-11-16 20:08:11 +0100

    client initial

---
+++ client/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+
+import { AppState } from '../app.service';
+import { Title } from './title';
+import { XLargeDirective } from './x-large';
+
+@Component({
+  selector: 'home',  
+  styleUrls: [ './home.component.css' ],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {
+  }
+}
