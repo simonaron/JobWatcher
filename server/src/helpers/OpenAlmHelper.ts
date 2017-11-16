8a81201a9b5a57b465c41c36b3386ef3248d2e97
Akos Makovics <akos.makovics@sigmatechnology.se>
2017-11-16 23:09:51 +0100

    Added Open ALM helper script

---
+++ server/src/helpers/OpenAlmHelper.ts
@@ -0,0 +1,110 @@
+import * as request from 'request';
+import { HttpHelper } from '../helpers/HttpHelper';
+
+let config = require('../../../config/user-config.json');
+
+let incidentTrackerId : number = 30453;
+let incidentVolume : number = 100;
+
+export class OpenAlmHelper {
+    
+
+    /*static async GetTrackerDefinition(id : number) {
+        let result = await HttpHelper.get(
+            config.urls.openalm + 'trackers/' + id);
+        return result;
+    }*/
+
+    static async GetJobArtifacts(jobName : string) 
+    {
+        var artifactData : string[][] = new Array();
+
+        let result = await HttpHelper.get(
+            config.urls.openalm + 'trackers/' +incidentTrackerId + '/artifacts?limit=' + incidentVolume + '&offset=0&order=desc');
+
+        //console.log(result[0]);
+        
+        var currentTitle : string;
+
+        for (var _i = 0; _i < result.length; _i++) 
+        {
+            currentTitle = result[_i].title;
+
+            if (currentTitle.indexOf(jobName) >= 0) 
+            {
+                artifactData.push(OpenAlmHelper.ProcessArtifact(result[_i]));
+                //console.log(OpenAlmHelper.ProcessArtifact(result[_i]));
+            }
+        }
+
+        return artifactData;
+    }
+
+    static async GetTrackerArtifacts(id : number, limit : number = 100, offset : number = 0, query : string = "") 
+    {
+        let result = await HttpHelper.get(
+            config.urls.openalm + 'trackers/' + id + '/artifacts?limit=' + limit + '&offset=' + offset + '&order=desc');
+        return result;
+    }
+
+    static async GetArtifactData(id : number)
+    {
+        let result = await HttpHelper.get(
+            config.urls.openalm + 'artifacts/' + id);
+        
+        return OpenAlmHelper.ProcessArtifact(result);
+    }
+
+    static ProcessArtifact(data : any) 
+    {
+        var artifactData : string[] = new Array();
+
+        artifactData[0] = data.title;
+        artifactData[1] = data.status;
+        artifactData[2] = data.submitted_on;
+        artifactData[3] = data.submitted_by_user.real_name;
+        artifactData[5] = 'https://openalm.lmera.ericsson.se/plugins/tracker/?aid=' + data.id;
+
+        return artifactData;
+    }
+
+    static GetBuildNumbers (title : string) 
+    {
+        var isBuildNumber : boolean = false;
+        var buildNumbers : string = "";
+
+        for (var i = 0; i < title.length; i++)
+        {
+            if (!isBuildNumber && (title[i] == '/' || title[i] == '#')) 
+            {
+                isBuildNumber = true;
+            }
+            else if (isBuildNumber)
+            {
+                //console.log(title[i]);
+                if (OpenAlmHelper.IsNumber(title[i]) == 1) 
+                {
+                    console.log(title[i]);
+                    buildNumbers += title[i];
+                }
+                else 
+                {
+                    if (title[i] == '/' || title[i] == '#' || title[i] == "-")
+                        buildNumbers += "||";
+                    else
+                        isBuildNumber = false;
+                }
+            }
+        }
+
+        var numbers = buildNumbers.split("||");
+
+        return numbers;
+    }
+
+    static IsNumber (character : string) 
+    {
+        var res = parseInt(character, 10);
+        return isNaN(res) ? null : 1;
+    }
+}
